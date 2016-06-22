503c9a15b7ab9374eff8bb370acb7680612a6d1a
Peter Scriven <justabitdope+git@gmail.com>
2016-06-22 14:22:35 +1200

    plays

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import roman_numeral_converter
+import logic
 import tkinter as tk
 import tkinter.ttk as ttk
 
 class NumeralConverterGUI(object):
+    
+    number_to_numeral = True;
+    
     def __init__(self, parent):
         self.parent = parent
         self.parent.minsize(350, 200)
@@ -11,7 +14,7 @@ class NumeralConverterGUI(object):
         
         # Mainframe
         self.main_frame = tk.Frame(self.parent, 
-                                   bg="#ffffff")
+                                   bg="#ff0fff")
         self.main_frame.pack(fill="both", expand=1)
         
         # TITLE
@@ -20,7 +23,7 @@ class NumeralConverterGUI(object):
                                     borderwidth=5)
         self.title = tk.Label(self.title_frame, 
                               text="Roman Numeral Converter", 
-                              font=("Raleway", "16"), 
+                              #font=("Raleway", "16"), 
                               justify="center", 
                               bg="#993388")
         self.title_frame.pack(fill="x", padx=10, pady=10)
@@ -29,16 +32,18 @@ class NumeralConverterGUI(object):
         # TEXTBOX / BUTTON
         self.input_var = tk.StringVar()
         self.entry_frame = tk.Frame(self.main_frame, 
-                                    borderwidth=10, 
-                                    bg="green")
+                                    bg="#ffffff",
+                                    borderwidth=10 
+                                    )
         self.entry_1 = ttk.Entry(self.entry_frame, 
                                  justify="center", 
                                  width=20, 
-                                 font=('Raleway', '16'),
+                                 #font=('Raleway', '16'),
                                  textvariable=self.input_var)
         self.button_1 = ttk.Button(self.entry_frame, 
                                    text="Convert", 
-                                   command=self.button_click)
+                                   command=self.button_click, 
+                                   default="normal")
         self.entry_frame.pack(fill="y", expand=1, padx=10, pady=0)
         self.entry_1.pack(pady=10)
         self.button_1.pack()
@@ -46,10 +51,10 @@ class NumeralConverterGUI(object):
         # OUTPUT
         self.output_var = tk.StringVar()
         self.output_frame = tk.Frame(self.main_frame, 
-                                     bg="red", 
+                                     bg="#ffffff", 
                                      borderwidth=5)
         self.label = ttk.Label(self.output_frame, 
-                               font=('Raleway', '16'),
+                               #font=('Raleway', '16'),
                                textvariable=self.output_var)
         self.output_frame.pack(fill="y", expand=1)
         self.label.pack()
@@ -61,8 +66,8 @@ class NumeralConverterGUI(object):
         text_in = self.input_var.get()
         text_out = ""
         
-        if roman_numeral_converter.check_number_input(text_in):
-            text_out = roman_numeral_converter.number_to_numeral(int(text_in))
+        if logic.check_number_input(text_in):
+            text_out = logic.number_to_numeral(int(text_in))
         
         self.input_var.set("")
         self.output_var.set(text_out)
